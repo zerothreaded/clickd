1f99b64ab76eebd4b79fbcb73f476d95848b07a5
ralph_masilamani <ralph_masilamani@bitbucket.org>
2014-02-19 17:38:12 +0000

    clear

--- applications/server/src/main/java/com/clickd/server/model/Answer.java
+++ applications/server/src/main/java/com/clickd/server/model/Answer.java
@@ -1,8 +1,5 @@
 package com.clickd.server.model;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.annotation.Id;
--- applications/server/src/main/java/com/clickd/server/services/questions/QuestionResource.java
+++ applications/server/src/main/java/com/clickd/server/services/questions/QuestionResource.java
@@ -1,32 +1,18 @@
-package com.clickd.server.services.application;
+package com.clickd.server.services.questions;
 
-import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.NewCookie;
-import javax.ws.rs.core.Response;
 
-import com.clickd.server.dao.SessionDao;
 import com.clickd.server.dao.QuestionDao;
-import com.clickd.server.model.Link;
-import com.clickd.server.model.Resource;
-import com.clickd.server.model.Session;
 import com.clickd.server.model.Question;
 import com.clickd.server.utilities.Utilities;
 import com.yammer.metrics.annotation.Timed;
