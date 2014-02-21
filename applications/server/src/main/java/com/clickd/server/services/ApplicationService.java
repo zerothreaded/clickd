f567d6dad035feab1407c49211893e32aefdabfe
ralph_masilamani <ralph_masilamani@bitbucket.org>
2014-02-21 00:21:57 +0000

    xx

--- applications/server/src/main/java/com/clickd/server/services/ApplicationService.java
+++ applications/server/src/main/java/com/clickd/server/services/ApplicationService.java
@@ -39,7 +39,7 @@ public class ApplicationService extends Service<UserConfiguration> {
 		bootstrap.addBundle(new AssetsBundle("/html/home", "/home", "index.html"));
 		bootstrap.addBundle(new AssetsBundle("/html/users", "/users/home", "index.html"));
 		bootstrap.addBundle(new AssetsBundle("/html/home", "/timeline", "timeline.html"));
-		}
+	}
 
 	@Override
 	public void run(UserConfiguration configuration, Environment environment) {
