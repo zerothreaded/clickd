c45588c31cd10c8a4af372a3480ff9f96ca4a566
ralph_masilamani <ralph_masilamani@bitbucket.org>
2014-03-12 19:51:57 +0000

    FUCK YOU

--- applications/server/src/main/java/com/clickd/server/services/ApplicationService.java
+++ applications/server/src/main/java/com/clickd/server/services/ApplicationService.java
@@ -53,8 +53,11 @@ public class ApplicationService extends Service<UserConfiguration> {
 
 		// context = new ClassPathXmlApplicationContext(new String[] { springContextFileName });
 		
-		String dataFolder = "c:\\sandbox\\data";
+		final String dataFolder = System.getProperty("dataFolder");
 		String fileName = dataFolder + "\\" + springContextFileName;
+		
+		System.out.println("\n\nUSER CONFIG FileNme = " + dataFolder);
+		
 		context = new FileSystemXmlApplicationContext(new String[] { fileName });
 
 		// Create REST End Points
