75ba14c6669fdfe489959650ec54f7b7c4594842
ralph_masilamani <ralph_masilamani@bitbucket.org>
2014-03-14 21:12:22 +0000

    x

--- applications/server/src/test/java/com/clickd/server/UtilitiesTest.java
+++ applications/server/src/test/java/com/clickd/server/UtilitiesTest.java
@@ -13,32 +13,32 @@ import org.junit.Test;
 
 public class UtilitiesTest {
 
-	private void copyFileUsingJava7Files(File source, File dest) throws IOException {
-	    Files.copy(source.toPath(), dest.toPath());
-	}
-
-	@Test
-	public void renameImages() throws Exception {
-		String dataDirectoryName = "C:\\sandbox\\data\\fromprod\\images\\users";
-		
-		File dataDirectory = new File(dataDirectoryName);
-		ArrayList<String> names = new ArrayList<String>(Arrays.asList(dataDirectory.list()));
-		for (String fileName : names) {
-			String decodedFileName = URLDecoder.decode(fileName);
-			System.out.println("File Name to translate [" + decodedFileName + "]");
-			StringTokenizer tokenizer = new StringTokenizer(decodedFileName, "\\");
-			while (tokenizer.hasMoreTokens()) {
-				String token = tokenizer.nextToken();
-				if (token.endsWith(".jpg")) {
-					File currentFile = new File(dataDirectoryName, decodedFileName);
-					File newFile = new File(dataDirectoryName, token);
-					boolean created = newFile.createNewFile();
-					// FileUtils.copyFile(currentFile, newFile);
-					copyFileUsingJava7Files(currentFile, newFile);
-
-				}
-				int wait;
-			}
-		}
-	}
+//	private void copyFileUsingJava7Files(File source, File dest) throws IOException {
+//	    Files.copy(source.toPath(), dest.toPath());
+//	}
+//
+//	@Test
+//	public void renameImages() throws Exception {
+//		String dataDirectoryName = "C:\\sandbox\\data\\fromprod\\images\\users";
+//		
+//		File dataDirectory = new File(dataDirectoryName);
+//		ArrayList<String> names = new ArrayList<String>(Arrays.asList(dataDirectory.list()));
+//		for (String fileName : names) {
+//			String decodedFileName = URLDecoder.decode(fileName);
+//			System.out.println("File Name to translate [" + decodedFileName + "]");
+//			StringTokenizer tokenizer = new StringTokenizer(decodedFileName, "\\");
+//			while (tokenizer.hasMoreTokens()) {
+//				String token = tokenizer.nextToken();
+//				if (token.endsWith(".jpg")) {
+//					File currentFile = new File(dataDirectoryName, decodedFileName);
+//					File newFile = new File(dataDirectoryName, token);
+//					boolean created = newFile.createNewFile();
+//					// FileUtils.copyFile(currentFile, newFile);
+//					copyFileUsingJava7Files(currentFile, newFile);
+//
+//				}
+//				int wait;
+//			}
+//		}
+//	}
 }
