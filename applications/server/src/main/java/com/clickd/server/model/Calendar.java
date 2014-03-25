2dd355c8ad37e9aadf5aef69815f6fcef7b7ba5a
ralph_masilamani <ralph_masilamani@bitbucket.org>
2014-03-25 12:32:46 +0000

    x

---
+++ applications/server/src/main/java/com/clickd/server/model/Calendar.java
@@ -0,0 +1,49 @@
+package com.clickd.server.model;
+
+import java.util.UUID;
+
+import org.springframework.data.annotation.Id;
+
+public class Calendar extends Resource {
+
+	@Id
+	protected String id;
+	protected String ref;
+	protected String name;
+	
+	public Calendar() {	
+		super();
+		createRef();
+	}
+
+	private void createRef() {
+		UUID uuid = UUID.randomUUID();
+		String ref = "/calendars/" + ((Long) Math.abs(uuid.getMostSignificantBits())).toString();
+		this.ref = ref;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getRef() {
+		return ref;
+	}
+	
+	public void setRef(String ref) {
+		this.ref = ref;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
