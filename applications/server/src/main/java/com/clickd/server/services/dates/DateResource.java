0ab127479815786a94feac794409fb88c4363389
ralph_masilamani <ralph_masilamani@bitbucket.org>
2014-03-25 15:03:35 +0000

    date

---
+++ applications/server/src/main/java/com/clickd/server/services/dates/DateResource.java
@@ -0,0 +1,56 @@
+package com.clickd.server.services.dates;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.clickd.server.dao.DateDao;
+import com.clickd.server.dao.UserDao;
+import com.clickd.server.model.Date;
+import com.clickd.server.model.ErrorMessage;
+import com.clickd.server.utilities.Utilities;
+import com.yammer.metrics.annotation.Timed;
+
+@Path("/dates")
+@Produces(MediaType.APPLICATION_JSON)
+public class DateResource {
+
+	@Autowired
+	private DateDao dateDao;
+	
+	@Autowired
+	private UserDao userDao;
+
+	@GET
+	@Timed
+	@Path("/{dateRef}")
+	public Response get(@PathParam("dateRef") String dateRef) {
+		try {
+			Date date = dateDao.findByRef("/dates/" + dateRef);
+			return Response.status(200).entity(Utilities.toJson(date)).build();
+		} catch(Exception e) {
+			e.printStackTrace();
+			return Response.status(300).entity(new ErrorMessage("failed", e.getMessage())).build();			
+		}
+	}
+	
+	@GET
+	@Timed
+	public Response getAll() {
+		try {
+			List<Date> allDates = dateDao.findAll();
+			return Response.status(200).entity(Utilities.toJson(allDates)).build();
+		} catch(Exception e) {
+			e.printStackTrace();
+			return Response.status(300).entity(new ErrorMessage("failed", e.getMessage())).build();			
+		}
+	}
+
+}
